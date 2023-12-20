734940bc2e7cb5eebc7331e28d0edaf97b76e49d
Latzi <latzi@i-matrix.com.au>
2023-12-20 15:44:15 +1000

    Update aligned_dataset.py  -Fixed flipping

--- data/aligned_dataset.py
+++ data/aligned_dataset.py
@@ -8,7 +8,6 @@ from PIL import Image
 import json
 from PIL import ImageDraw
 
-
 class AlignedDataset(BaseDataset):
     def initialize(self, opt):
         self.opt = opt
@@ -16,7 +15,6 @@ class AlignedDataset(BaseDataset):
         self.dir_AB = os.path.join(opt.dataroot, 'images', opt.phase)
         self.dir_bbox = os.path.join(opt.dataroot, 'bbox', opt.phase)
 
-        #self.AB_paths, self.bbox_paths = sorted(make_dataset(self.dir_AB, self.dir_bbox))
         self.AB_paths, self.bbox_paths = make_dataset(self.dir_AB, self.dir_bbox)
         self.AB_paths = sorted(self.AB_paths)
         self.bbox_paths = sorted(self.bbox_paths)
@@ -29,6 +27,7 @@ class AlignedDataset(BaseDataset):
 
         self.transform = transforms.Compose(transform_list)
 
+    @staticmethod
     def draw_bbox(image, bbox, color='red', width=3):
         """Draw a bounding box on the image."""
         draw = ImageDraw.Draw(image)
@@ -57,7 +56,7 @@ class AlignedDataset(BaseDataset):
         if not isinstance(bbox, dict):
            raise TypeError(f"Bounding box data is not a dictionary at index {index}: {bbox}")
 
-    # Extract the bounding box values
+        # Extract the bounding box values
         try:
            bbox_y = int(bbox.get('y', 0))
            bbox_x = int(bbox.get('x', 0))
@@ -66,45 +65,32 @@ class AlignedDataset(BaseDataset):
         except ValueError as e:
            raise ValueError(f"Error processing bbox values at index {index}: {e}")
 
-        """print(f"haha\nDataset bbox (index {index}): [{bbox_y}, {bbox_x}, {bbox_w}, {bbox_h}]")  # Debugging print"""
-
         bbox_x = max(int((bbox_x / self.opt.fineSize) * self.opt.loadSize), 0)
         bbox_y = max(int((bbox_y / self.opt.fineSize) * self.opt.loadSize), 0)
         bbox_w = max(int((bbox_w / self.opt.fineSize) * self.opt.loadSize), 0)
         bbox_h = max(int((bbox_h / self.opt.fineSize) * self.opt.loadSize), 0)
 
-        if bbox_y <= h_offset or bbox_x <= w_offset:
-            AB = Image.open(AB_path).convert('RGB')
-            AB = AB.resize((self.opt.fineSize * 2, self.opt.fineSize), Image.BICUBIC)
-            AB = self.transform(AB)
-            A = AB[:, :self.opt.fineSize, :self.opt.fineSize]
-            B = AB[:, :self.opt.fineSize, self.opt.fineSize:2 * self.opt.fineSize]
-        else:
-            AB = Image.open(AB_path).convert('RGB')
-            AB = AB.resize((self.opt.loadSize * 2, self.opt.loadSize), Image.BICUBIC)
-            AB = self.transform(AB)
-            A = AB[:, h_offset:h_offset + self.opt.fineSize, w_offset:w_offset + self.opt.fineSize]
-            B = AB[:, h_offset:h_offset + self.opt.fineSize, w + w_offset:w + w_offset + self.opt.fineSize]
+        AB = Image.open(AB_path).convert('RGB')
+        AB = AB.resize((w_total, h), Image.BICUBIC)
+        AB = self.transform(AB)
 
-        return {'A': A, 'B': B, 'bbox': [bbox_y, bbox_x, bbox_w, bbox_h], 'A_paths': AB_path, 'B_paths': AB_path}
+        A = AB[:, h_offset:h_offset + self.opt.fineSize, w_offset:w_offset + self.opt.fineSize]
+        B = AB[:, h_offset:h_offset + self.opt.fineSize, w + w_offset:w + w_offset + self.opt.fineSize]
 
+        bbox_list = [bbox_y, bbox_x, bbox_w, bbox_h]
 
+        
         if (not self.opt.no_flip) and random.random() < 0.5:
             idx = [i for i in range(A.size(2) - 1, -1, -1)]
             idx = torch.LongTensor(idx)
             A = A.index_select(2, idx)
             B = B.index_select(2, idx)
-            #print A.size(2)
-            bbox = [bbox[0], A.size(2) - bbox[2], A.size(2) - bbox[1], bbox[3]]
-            #print('hehe')
-            #print(bbox)
-            #print(A.size())
-            #print(bbox)
-        return {'A': A, 'B': B, 'bbox': bbox,
-                'A_paths': AB_path, 'B_paths': AB_path}
-            
+            bbox_list = [bbox_list[0], A.size(2) - bbox_list[2], A.size(2) - bbox_list[1], bbox_list[3]]
+            #print(f"-----------Image at index {index} flipped.------------------")
+        #else:
+            #print(f"No flipping for index {index}")
 
-     
+        return {'A': A, 'B': B, 'bbox': bbox_list, 'A_paths': AB_path, 'B_paths': AB_path}
 
     def __len__(self):
         return len(self.AB_paths)
